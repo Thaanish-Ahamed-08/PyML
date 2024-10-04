1ca8826f9ab1515df8124cdb3b67e73b1791858c
Thaanish <thaanish.ahmed@gmail.com>
2024-10-04 23:22:55 +0530

    Removed Unnecessary block of codes

--- AutoML.py
+++ AutoML.py
@@ -47,50 +47,6 @@ def train_and_evaluate(model, X_train, y_train, X_test, y_test, metric):
     y_test = y_test.detach().numpy()
     
     return metric(y_test, predictions), loss_history
-    
-def hyperparameter_tuning(model, param_grid, X_train, y_train):
-    best_params = None
-    best_score = float('-inf')
-    for params in param_grid:
-        model.set_params(**params)  # Assume your model has set_params
-        score = cross_validation(model, X_train, y_train)
-        if score > best_score:
-            best_score = score
-            best_params = params
-    return best_params
-
-def cross_validation(model, X, y, cv=5):
-    kfold = KFold(n_splits=cv)
-    scores = []
-    
-    for train_idx, val_idx in kfold.split(X):
-        X_train_fold, X_val_fold = X[train_idx], X[val_idx]
-        y_train_fold, y_val_fold = y[train_idx], y[val_idx]
-        
-        model.fit(X_train_fold, y_train_fold)
-        predictions = model.predict(X_val_fold)
-        accuracy = accuracy_score(y_val_fold.numpy(), predictions.numpy())
-        scores.append(accuracy)
-    
-    return np.mean(scores)
-
-class StackingEnsemble:
-    def __init__(self, base_models, meta_model):
-        self.base_models = base_models
-        self.meta_model = meta_model
-
-    def fit(self, X, y):
-        base_predictions = []
-        for model in self.base_models:
-            model.fit(X, y)
-            base_predictions.append(model.predict(X).detach().numpy())  # Convert to numpy
-        meta_input = np.column_stack(base_predictions)
-        self.meta_model.fit(meta_input, y)
-
-    def predict(self, X):
-        base_predictions = [model.predict(X).detach().numpy() for model in self.base_models]
-        meta_input = np.column_stack(base_predictions)
-        return self.meta_model.predict(meta_input)
 
 def evaluate_model(predictions, ground_truth, metrics):
     results = {}
